1779dd90b9604e380295ce674abaec7e0e7e07da
peng49 <1219955253@qq.com>
2022-09-01 14:11:57 +0800

    添加gocron-node

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# 启动php-fpm
+php-fpm --daemonize
+
+# 已守护进程的方式启动nginx
+nginx
+
+# 如果存在gocron-node,自动启动
+if [ -f /app/gocron-node ]
+then
+  nohup /app/gocron-node -allow-root > /var/log/gocron-node.log 2>&1 &
+fi
+
+tail -f /var/log/nginx/access.log
